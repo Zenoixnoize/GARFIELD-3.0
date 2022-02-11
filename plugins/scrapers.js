501320a3e544a795724315d07939e315c8550481
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-02-11 13:55:11 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -301,7 +301,7 @@ if (config.WORKTYPE == 'private') {
                 return;
     
             let 
-                LANG = 'ml',
+                LANG = 'si',
                 ttsMessage = match[1],
                 SPEED = 1.0
 
@@ -328,7 +328,7 @@ if (config.WORKTYPE == 'private') {
                 return;
     
             let 
-                LANG = 'ml',
+                LANG = 'si',
                 ttsMessage = match[1],
                 SPEED = 1.0
 
@@ -356,14 +356,14 @@ if (config.WORKTYPE == 'private') {
         if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
         var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_SONG,MessageType.text);
 
-        let title = arama[0].title.replace(' ', '+');
-        let stream = ytdl(arama[0].videoId, {
+        let title = arama[1].title.replace(' ', '+');
+        let stream = ytdl(arama[1].videoId, {
             quality: 'highestaudio',
         });
     
         got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
         ffmpeg(stream)
-            .audioBitrate(320)
+            .audioBitrate(64)
             .save('./' + title + '.mp3')
             .on('end', async () => {
                 const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
@@ -386,7 +386,7 @@ if (config.WORKTYPE == 'private') {
             const p_lk = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:' + Pinky.OA_NAME + '\n' //created afnanplk, please copy this with credit..
-            + 'ORG:pinky julie fam;\n' 
+            + 'ORG:A project by NOIZE;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=' + Pinky.PHONE + ':' + Pinky.PHONE + ' \n'
             + 'END:VCARD'
 await message.client.sendMessage(message.jid, {displayname: "PINKY", vcard: p_lk}, MessageType.contact);
@@ -807,7 +807,7 @@ else if (config.WORKTYPE == 'public') {
             return;
     
         let 
-            LANG = 'ml',
+            LANG = 'si',
             ttsMessage = match[1],
             SPEED = 1.0
 
@@ -835,27 +835,28 @@ else if (config.WORKTYPE == 'public') {
         if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
         var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_SONG,MessageType.text);
 
-        let title = arama[0].title.replace(' ', '+');
-        let stream = ytdl(arama[0].videoId, {
-            quality: 'highestaudio',
+        let title = arama[1].title.replace(' ', '+');
+        let stream = ytdl(arama[1].videoId, {
+            quality: 'lowestaudio',
         });
     
-        got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
+        got.stream(arama[1].image).pipe(fs.createWriteStream(title + '.jpg'));
         ffmpeg(stream)
-            .audioBitrate(320)
+            .audioBitrate(128)
             .save('./' + title + '.mp3')
+            .save(title + '.jpg')
             .on('end', async () => {
                 const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
-                writer.setFrame('TIT2', arama[0].title)
-                    .setFrame('TPE1', [arama[0].author.name])
+                writer.setFrame('TIT2', arama[1].title)
+                    .setFrame('TPE1', [arama[1].author.name])
                     .setFrame('APIC', {
                         type: 3,
                         data: fs.readFileSync(title + '.jpg'),
-                        description: arama[0].description
+                        description: arama[1].description
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
+                reply = await message.client.sendMessage(message.jid,'```Garfield Uploading song... 🐼```' + title + title + '.jpg',MessageType.image);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {quoted: message.data , mimetype: Mimetype.mp4Audio, ptt: false});
             });
     }));
